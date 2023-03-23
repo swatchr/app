aaaf6a470901db93e5bdb045bf642fc5dab0ac7a
gshah2020 <gaurang.r.shah@gmail.com>
2023-03-23 11:12:37 -0400

    add double-click to history viewer

--- src/components/v1/palette/panels/history-viewer.tsx
+++ src/components/v1/palette/panels/history-viewer.tsx
@@ -14,6 +14,7 @@ export function HistoryViewer({
 }) {
   const [show, setShow] = useState(false);
   const { text } = useThemeColors();
+  const { history } = colorHandlers;
 
   return (
     <VStack
@@ -65,6 +66,11 @@ export function HistoryViewer({
                 colorHandlers.history.historyIndex === i ? '1px solid' : 'none'
               }
               borderColor={borderColor}
+              cursor="pointer"
+              onDoubleClick={(e: React.MouseEvent<HTMLDivElement>) => {
+                e.stopPropagation();
+                colorHandlers.history.handleChange(color);
+              }}
             />
           );
         })}
