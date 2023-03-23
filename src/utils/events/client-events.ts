05bce5be4551a59a17b1d50cba0b85009bf24db0
gshah2020 <gaurang.r.shah@gmail.com>
2023-03-23 14:54:21 -0400

    update event handling

--- src/utils/events/client-events.ts
+++ src/utils/events/client-events.ts
@@ -1,24 +1,43 @@
-import type { Maybe } from '@/types';
 // @SEE: https://blog.logrocket.com/using-custom-events-react/
 
 // @NOTE: CustomEvent is not supported in IE11
 
-type listenerFn = ({ ...args }: Maybe['object']) => any;
-type EventFn = (eventName: string, listener: EventListener) => void;
+type CustomEventOptions = CustomEventInit & {
+  detail?: Record<string, any>;
+};
 
-function subscribe(eventName: string, listener: listenerFn) {
-  document.addEventListener(eventName, listener);
-}
+type ListenerFn<T extends CustomEventOptions> = (
+  event: CustomEvent<T> & { target: Document }
+) => void;
+type SubscribeFn<T extends CustomEventOptions> = (
+  eventName: string,
+  listener: ListenerFn<T>
+) => void;
+type UnsubscribeFn<T extends CustomEventOptions> = (
+  eventName: string,
+  listener: ListenerFn<T>
+) => void;
+type PublishFn<T extends CustomEventOptions> = (
+  eventName: string,
+  data?: Record<string, any>
+) => void;
 
-function unsubscribe(eventName: string, listener: listenerFn) {
-  document.removeEventListener(eventName, listener);
-}
+export const subscribe: SubscribeFn<CustomEventOptions> = (
+  eventName,
+  listener
+) => {
+  document.addEventListener(eventName, listener as EventListener);
+};
 
-function publish(eventName: string, data: Record<string, any>) {
-  // @NOTE: publish data is accessible from the options.details property to subscribers
-  const options = data && Object.keys(data).length ? { detail: data } : {};
+export const unsubscribe: UnsubscribeFn<CustomEventOptions> = (
+  eventName,
+  listener
+) => {
+  document.removeEventListener(eventName, listener as EventListener);
+};
+
+export const publish: PublishFn<CustomEventOptions> = (eventName, data) => {
+  const options: CustomEventOptions = data ? { detail: data } : {};
   const event = new CustomEvent(eventName, options);
   document.dispatchEvent(event);
-}
-
-export { publish, subscribe, unsubscribe };
+};
