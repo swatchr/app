65e38820ce97e969728089a7fb5e46ec305135f0
gshah2020 <gaurang.r.shah@gmail.com>
2023-03-24 19:19:04 -0400

    promote server-side loading

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -1,6 +1,7 @@
+import { useRouter } from 'next/router';
 import { useCallback, useEffect, useState } from 'react';
 
-import type { GetServerSideProps, NextPage } from 'next';
+import type { NextPage } from 'next';
 
 import { BaseLayout, Palette } from '@/components';
 import { SocialShare } from '@/components/v1/_scaffold/social';
@@ -11,14 +12,38 @@ import { FullScreenLoader } from 'chakra.ui';
 import { shortname } from 'lib/unique-names-generator';
 import { encodeQueryParams } from '../utils/fns';
 
-interface Props {
-  ogImageUrl: string;
-  paletteName: string;
-  colorParams?: string[];
-}
-
-const Sandbox: NextPage<Props> = ({ ogImageUrl, paletteName, colorParams }) => {
+const Sandbox: NextPage = () => {
+  const router = useRouter();
   const { isLoading } = useIsLoading(true, 200);
+
+  let colorParams: string[] | undefined = undefined;
+  let paletteName: string = shortname();
+
+  // If query parameters are present, validate them
+  if (router?.query) {
+    const { colors, name } = router.query;
+    // Validate the color parameter
+    if (colors && typeof colors === 'string') {
+      const parsedColors = parsePalette(colors);
+      if (Array.isArray(parsedColors)) {
+        colorParams = parsedColors;
+      } else {
+        console.error(`Invalid color parameter: ${colors}`);
+      }
+    } else if (colors) {
+      console.error(`Invalid color parameter: ${colors}`);
+    }
+    // Validate the palette name
+    if (name && typeof name === 'string') {
+      paletteName = slugify(name.substring(0, 20));
+    } else if (name) {
+      console.error(`Invalid palette name: ${name}`);
+    }
+  }
+
+  const ogImageUrl = `${getBuildUrl()}/api/og?${encodeQueryParams(
+    router.query
+  )}`;
 
   return (
     <BaseLayout
@@ -53,46 +78,6 @@ const Sandbox: NextPage<Props> = ({ ogImageUrl, paletteName, colorParams }) => {
 };
 
 export default Sandbox;
-
-export const getServerSideProps: GetServerSideProps<Props> = async ({
-  query,
-}) => {
-  let colorParams: string[] | undefined = undefined;
-  let paletteName: string = shortname();
-
-  // If query parameters are present, validate them
-  if (query) {
-    const { colors, name } = query;
-    // Validate the color parameter
-    if (colors && typeof colors === 'string') {
-      const parsedColors = parsePalette(colors);
-      if (Array.isArray(parsedColors)) {
-        colorParams = parsedColors;
-      } else {
-        console.error(`Invalid color parameter: ${colors}`);
-      }
-    } else if (colors) {
-      console.error(`Invalid color parameter: ${colors}`);
-    }
-    // Validate the palette name
-    if (name && typeof name === 'string') {
-      paletteName = slugify(name.substring(0, 20));
-      // paletteName = name.substring(0, 20).replace(/[^a-zA-Z0-9-]/g, '');
-    } else if (name) {
-      console.error(`Invalid palette name: ${name}`);
-    }
-  }
-
-  const ogImageUrl = `${getBuildUrl()}/api/og?${encodeQueryParams(query)}`;
-
-  return {
-    props: {
-      colorParams,
-      paletteName,
-      ogImageUrl,
-    },
-  };
-};
 
 function useIsLoading(initialValue: boolean, resetInterval: number) {
   const [isLoading, setIsLoading] = useState(initialValue);
