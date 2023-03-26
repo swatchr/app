a7c1f33e61c4042914295c45645b1de33636fecb
gshah2020 <gaurang.r.shah@gmail.com>
2023-03-26 12:08:04 -0400

    chore: localize storage keys

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,18 +1,6 @@
-// time helpers
-import { env } from '@/env.mjs';
-export const ONE_SECOND_MS = 1000;
-export const ONE_MIN_MS = 60 * ONE_SECOND_MS;
-export const ONE_HOUR_MS = 60 * ONE_MIN_MS;
-export const ONE_DAY_MS = 24 * ONE_HOUR_MS;
-// config settings
-export const PORT = 3000;
-export const ACCESS_TOKEN_EXPIRES_IN = 15;
-export const REFRESH_TOKEN_EXPIRES_IN = 60;
-export const REDIS_CACHE_EXPIRES_INT = 60;
-// export const MAX_BATCH_SIZE = 10;
-
-export const DEFAULT_STALE_TIME: number = 5 * ONE_SECOND_MS; // 5secs
-
+/* -------------------------------------------------------------------------- */
+/*                                ENVIRONMENTS                                */
+/* -------------------------------------------------------------------------- */
 export const isBrowser = typeof window !== 'undefined';
 export const isProd = process.env.NODE_ENV === 'production';
 export const isDev = process.env.NODE_ENV === 'development';
@@ -21,30 +9,39 @@ export const isClient = !!(
   window.document &&
   window.document.createElement
 );
-
 export const TEST_ENV = process.env.NEXT_PUBLIC_APP_ENV === 'test';
-
 export const IS_TEST = TEST_ENV || process.env.NODE_ENV === 'test';
 
-export const debug = isDev && false;
+/* -------------------------------------------------------------------------- */
+/*                                TIME HELPERS                                */
+/* -------------------------------------------------------------------------- */
+export const ONE_SECOND_MS = 1000;
+export const ONE_MIN_MS = 60 * ONE_SECOND_MS;
+export const ONE_HOUR_MS = 60 * ONE_MIN_MS;
+export const ONE_DAY_MS = 24 * ONE_HOUR_MS;
 
+/* -------------------------------------------------------------------------- */
+/*                             REACT QUERY CONFIG                             */
+/* -------------------------------------------------------------------------- */
+export const PORT = 3000;
+export const ACCESS_TOKEN_EXPIRES_IN = 15;
+export const REFRESH_TOKEN_EXPIRES_IN = 60;
+export const REDIS_CACHE_EXPIRES_INT = 60;
+// export const MAX_BATCH_SIZE = 10;
+export const DEFAULT_STALE_TIME: number = 5 * ONE_SECOND_MS; // 5secs
+
+/* -------------------------------------------------------------------------- */
+/*                                STORAGE KEYS                                */
+/* -------------------------------------------------------------------------- */
+export const ANON_ID_KEY = '__anon_id';
 export const NEXT_AUTH_SESSION_TOKEN_KEY = 'next-auth.session-token';
+export const APP_CONSENT = 'app-consent';
 
-// image cdn link components -- still used by:
-// - components/blocks/about/license-info.tsx
-// - components/blocks/about/license-stack.tsx
-
-export const CDN_URL = 'https://cdn.jsdelivr.net/gh/rupistudio/assets@main';
-export const LOCATIONS_DIR = '/images/locations';
-export const BRAND_DIR = '/brand';
-export const PEOPLE_DIR = '/images/people';
-export const PLACEHOLDER_DIR = '/images/placeholder';
-export const SERVICES_DIR = '/images/services';
-
+export const PALETTE_KEY = 'palette';
+export const PALETTE_NAME_KEY = 'palette-name';
 /* -------------------------------------------------------------------------- */
 /*                                    REGEX                                   */
 /* -------------------------------------------------------------------------- */
-
 export const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 export const PHONE_REGEX = /^\d{3}-\d{3}-\d{4}$/;
 export const ZIP_REGEX = /^\d{5}(?:[-\s]\d{4})?$/;
@@ -75,3 +72,5 @@ export const HEX_COLOR_WITHOUT_HASH_REGEX = /^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
 export const HEX_COLOR_REGEX_STRING = '^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$';
 export const HEX_COLOR_WITHOUT_HASH_REGEX_STRING =
   '^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$';
+
+export const debug = isDev && false;
