3fa5b4373420b2eaa6bfaaceb09df95ed0095aa2
gshah2020 <gaurang.r.shah@gmail.com>
2023-03-26 00:22:50 -0400

    remove flex expand

--- src/components/v1/palette/swatch/swatch.tsx
+++ src/components/v1/palette/swatch/swatch.tsx
@@ -55,88 +55,86 @@ export function Swatch({ index }: { index: number }) {
   }, []);
 
   return (
-    <AnimatePresence>
-      <Flex
-        flex={activeSwatchIndex === index ? 1 : 0}
-        direction="column"
-        className="swatch-wrapper"
-        position="relative"
-        w="full"
-        h="100vh"
-        my="auto"
-        mx="auto"
-        justifyContent="center"
-        alignItems="center"
-        as={motion.div}
-        animate={controls}
-        onMouseEnter={() => {
-          colorHandlers.paletteHandlers.activateSwatch(index);
-          swatchMenuOnOpen();
-        }}
-        onMouseLeave={() => {
-          // colorHandlers.paletteHandlers.activateSwatch(-1);
-          swatchMenuOnClose();
-        }}
-        willChange={'flex'}
-        transition="flex 500ms cubic-bezier(0.645, 0.045, 0.355, 1.000)"
-        color={text} // all of the icons and text inherit this text color
-      >
-        <SlideFade in={mounted} offsetX={-96} reverse unmountOnExit>
+    <Flex
+      // flex={activeSwatchIndex === index ? 1 : 0}
+      direction="column"
+      className="swatch-wrapper"
+      position="relative"
+      w="full"
+      h="100vh"
+      my="auto"
+      mx="auto"
+      justifyContent="center"
+      alignItems="center"
+      as={motion.div}
+      animate={controls}
+      onMouseEnter={() => {
+        colorHandlers.paletteHandlers.activateSwatch(index);
+        swatchMenuOnOpen();
+      }}
+      onMouseLeave={() => {
+        // colorHandlers.paletteHandlers.activateSwatch(-1);
+        swatchMenuOnClose();
+      }}
+      willChange={'flex'}
+      transition="flex 500ms cubic-bezier(0.645, 0.045, 0.355, 1.000)"
+      color={text} // all of the icons and text inherit this text color
+    >
+      <SlideFade in={mounted} offsetX={-96} reverse unmountOnExit>
+        <Center
+          className="swatch-wrapper"
+          boxSize={72}
+          position="relative"
+          zIndex={1}
+        >
+          {swatchMenuIsOpen ? (
+            <SwatchMenu
+              colorState={colorState}
+              colorHandlers={colorHandlers}
+              reset={reset}
+            />
+          ) : null}
+          <SwatchWindow isActive={swatchMenuIsOpen} />
           <Center
-            className="swatch-wrapper"
-            boxSize={72}
+            as={motion.div}
+            className="picker-swatch"
             position="relative"
-            zIndex={1}
+            role="button"
+            initial={{ boxShadow: '0px 0px 20px rgba(0,0,0, 0)' }}
+            animate={controls}
+            whileHover={{ boxShadow: '0px 0px 20px rgba(0,0,0, 0.1)' }}
+            whileTap={{ boxShadow: '0px 0px 20px rgba(0,0,0, 0.1)' }}
+            transition="0.55 easeInOut 0.3"
+            boxSize={52}
+            tabIndex={0}
+            zIndex={0}
+            rounded="xl"
+            onClick={switchToPickerView}
           >
-            {swatchMenuIsOpen ? (
-              <SwatchMenu
-                colorState={colorState}
+            {view === 'picker' ? (
+              <ColorPickerWrapper
+                color={colorState.color}
                 colorHandlers={colorHandlers}
-                reset={reset}
+                view={view}
+                onClick={switchView}
               />
             ) : null}
-            <SwatchWindow isActive={swatchMenuIsOpen} />
-            <Center
-              as={motion.div}
-              className="picker-swatch"
-              position="relative"
-              role="button"
-              initial={{ boxShadow: '0px 0px 20px rgba(0,0,0, 0)' }}
-              animate={controls}
-              whileHover={{ boxShadow: '0px 0px 20px rgba(0,0,0, 0.1)' }}
-              whileTap={{ boxShadow: '0px 0px 20px rgba(0,0,0, 0.1)' }}
-              transition="0.55 easeInOut 0.3"
-              boxSize={52}
-              tabIndex={0}
-              zIndex={0}
-              rounded="xl"
-              onClick={switchToPickerView}
-            >
-              {view === 'picker' ? (
-                <ColorPickerWrapper
-                  color={colorState.color}
-                  colorHandlers={colorHandlers}
-                  view={view}
-                  onClick={switchView}
-                />
-              ) : null}
-              {view == 'swatch' ? (
-                <EditableHexInput
-                  show={swatchMenuIsOpen}
-                  colorState={colorState}
-                  handleChange={colorHandlers.history.handleChange}
-                />
-              ) : null}
-            </Center>
+            {view == 'swatch' ? (
+              <EditableHexInput
+                show={swatchMenuIsOpen}
+                colorState={colorState}
+                handleChange={colorHandlers.history.handleChange}
+              />
+            ) : null}
           </Center>
-        </SlideFade>
-        <InfoPanel {...colorState} showIcon={swatchMenuIsOpen} />
-        {colorState.isActive ? (
-          <>
-            <HistoryViewer colorHandlers={colorHandlers} />
-          </>
-        ) : null}
-      </Flex>
-    </AnimatePresence>
+        </Center>
+      </SlideFade>
+      <InfoPanel {...colorState} showIcon={swatchMenuIsOpen} />
+      {colorState.isActive ? (
+        <>
+          <HistoryViewer colorHandlers={colorHandlers} />
+        </>
+      ) : null}
+    </Flex>
   );
 }
