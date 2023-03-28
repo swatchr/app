30e5f4f468f77ebe3a02e6bd1188e06136293750
gshah2020 <gaurang.r.shah@gmail.com>
2023-03-28 14:52:30 -0400

    Fixes SWT-53: update export panel code formatting

--- src/components/v1/palette/panels/export-panel.tsx
+++ src/components/v1/palette/panels/export-panel.tsx
@@ -1,13 +1,13 @@
 import {
   Button,
   ButtonGroup,
+  Code,
   HStack,
   Tab,
   TabList,
   TabPanel,
   TabPanels,
   Tabs,
-  Textarea,
   useColorMode,
 } from '@chakra-ui/react';
 
@@ -88,65 +88,92 @@ export function ExportPanel({
         <TabPanels fontFamily="mono">
           {Object.keys(exportItems).map((key) => {
             const item = exportItems[key as keyof typeof exportItems];
-            const contrast = colorMode === 'light' ? 'gray.200' : 'gray.800';
-            // eslint-disable-next-line react-hooks/rules-of-hooks
-            const { isCopied, copy } = useClipboard({
-              text: item.stringify(palette),
-              onCopy: () => {
-                publish('show-toast', {
-                  id: `export-${key}-copied`,
-                  title: 'Copied to clipboard',
-                  description: item.description || 'Sorry. Please try again',
-                  status: 'success',
-                  duration: 1000,
-                  isClosable: true,
-                });
-              },
-            });
+            console.log('🚀 | file: export-panel.tsx:109 | item:', item);
             return (
-              <TabPanel key={key} position="relative">
-                <Textarea
-                  defaultValue={item.stringify(palette)}
-                  whiteSpace="pre"
-                  rows={5}
-                  isReadOnly
-                  bg={contrast}
-                  p={8}
-                  border="1px"
-                  borderColor={contrast}
-                  rounded="lg"
-                  onClick={copy}
-                  cursor="pointer"
-                />
-
-                <ButtonGroup
-                  as={HStack}
-                  w="full"
-                  mt={8}
-                  justifyContent="flex-end"
-                  gap={3}
-                  size="sm"
-                >
-                  <Button
-                    leftIcon={isCopied ? <CheckIcon /> : <CopyIcon />}
-                    colorScheme={scheme}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      e.stopPropagation();
-                      copy(e);
-                    }}
-                  >
-                    {isCopied ? 'Copied' : 'Copy'}
-                  </Button>
-                  <Button colorScheme="gray" onClick={onClose}>
-                    Close
-                  </Button>
-                </ButtonGroup>
-              </TabPanel>
+              <CodeBlockPanels
+                key={key}
+                item={item}
+                palette={palette}
+                colorMode={colorMode}
+                scheme={scheme}
+                onClose={onClose}
+              />
             );
           })}
         </TabPanels>
       </Tabs>
     </CHModal>
+  );
+}
+
+export function CodeBlockPanels({
+  key,
+  palette,
+  item,
+  colorMode,
+  scheme,
+  onClose,
+}: {
+  key: string;
+  palette: string[];
+  item: any;
+  colorMode: string;
+  scheme: string;
+  onClose: () => void;
+}) {
+  const contrast = colorMode === 'light' ? 'gray.200' : 'gray.800';
+  const { isCopied, copy } = useClipboard({
+    text: item?.stringify(palette).toString(),
+    onCopy: () => {
+      publish('show-toast', {
+        id: `export-${key}-copied`,
+        title: 'Copied to clipboard',
+        description: item.description || 'Sorry. Please try again',
+        status: 'success',
+        duration: 1000,
+        isClosable: true,
+      });
+    },
+  });
+  return (
+    <TabPanel key={key} position="relative">
+      <Code
+        w="full"
+        whiteSpace="pre"
+        bg={contrast}
+        p={8}
+        border="1px"
+        borderColor={contrast}
+        rounded="lg"
+        onClick={copy}
+        cursor="pointer"
+      >
+        {item?.stringify(palette).toString()}
+      </Code>
+
+      <ButtonGroup
+        as={HStack}
+        w="full"
+        mt={8}
+        justifyContent="flex-end"
+        gap={3}
+        size="sm"
+      >
+        <Button
+          leftIcon={isCopied ? <CheckIcon /> : <CopyIcon />}
+          colorScheme={scheme}
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            copy(e);
+          }}
+        >
+          {isCopied ? 'Copied' : 'Copy'}
+        </Button>
+        <Button colorScheme="gray" onClick={onClose}>
+          Close
+        </Button>
+      </ButtonGroup>
+    </TabPanel>
   );
 }
--- src/utils/swatchr.ts
+++ src/utils/swatchr.ts
@@ -24,50 +24,68 @@ const colorProperties = [
 ];
 
 export function cssStringifyPalette(palette: string[]): string {
-  const string = `:root {
-    ${palette
-      .map((color, index) => `--${colorProperties[index]}: ${color}`)
-      .join(';\n  ')}
-  }`;
-  return string;
+  const endResult = palette.reduce(
+    (acc: Record<string, string>, color: string, index: number) => {
+      if (colorProperties[index]) {
+        acc[`--${colorProperties[index]}`] = color;
+      }
+      return acc;
+    },
+    {}
+  );
+
+  const endResultString = JSON.stringify(endResult, null, 2)
+    .replace(/"/g, '')
+    .replace(/,/g, ';')
+    .replace(/^{/, ':root {  ')
+    .replace(/}$/, '}');
+
+  return endResultString;
 }
 
 export function tailwindStringifyPalette(palette: string[]) {
-  const themeColors = {} as { [key: string]: string };
-  palette.forEach((color, index) => {
-    themeColors[`${index + 1}00`] = color;
-  });
-  return JSON.stringify({ colors: themeColors }, null, 2);
+  const tailwindColors: Record<string, string> = palette.reduce(
+    (acc, color, index) => {
+      // @ts-expect-error implicit-any
+      acc[index + 1 + '00'] = color;
+      return acc;
+    },
+    {}
+  );
+  return JSON.stringify({ colors: tailwindColors }, null, 2);
 }
 
-export function objectStringifyPalette(palette: string[]): string {
-  const string = `const colors = ${JSON.stringify(
-    palette
-      .map((color: string, index: number) => {
-        const obj: { [key: string]: string } = {};
-        obj[colorProperties[index]!] = color;
-        return obj;
-      })
-      .reduce((result, currentObj) => ({ ...result, ...currentObj }), {}),
-    null,
-    2
-  )}`;
-  return string;
+export function objectStringifyPalette(
+  palette: string[]
+  // outputFormat = 'js'
+): string | { [key: string]: string } {
+  const endResult = palette.reduce((acc, color, index) => {
+    const key = colorProperties[index];
+    if (key) {
+      // @ts-expect-error implicit-any
+      acc[key] = color;
+    }
+    return acc;
+  }, {});
+
+  // return outputFormat === 'json'
+  //   ? JSON.stringify(endResult, null, 2)
+  //   : endResult;
+  return JSON.stringify(endResult, null, 2);
 }
 
 export function sysUIStringifyPalette(palette: string[]): string {
-  const colors = {} as { [key: string]: string };
-  palette.forEach((color, index) => {
-    colors[`${index + 1}00`] = color;
-  });
-  return `const colors = ${JSON.stringify(colors, null, 2)} `;
+  const colors = palette.reduce((acc, color, index) => {
+    // @ts-expect-error implicit-any
+    acc[index + 1 + '00'] = color;
+    return acc;
+  }, {});
+  return `const colors = ${JSON.stringify(colors, null, 2)};`;
 }
 
 export function scssStringifyPalette(palette: string[]): string {
-  const string = `$colors: (
-    ${palette
-      .map((color, index) => `${colorProperties[index]}: ${color},`)
-      .join('\n\t')}
-  );`;
+  const string = `$colors: (\n${palette
+    .map((color, index) => `  ${colorProperties[index]}: ${color}`)
+    .join(',\n')}\n);`;
   return string;
 }
