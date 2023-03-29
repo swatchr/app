9c5a5b6fe67c4fc10b4f9fd31db1e22cc5db44e6
gshah2020 <gaurang.r.shah@gmail.com>
2023-03-29 02:22:43 -0400

    add brand fonts + update system fallbacks

--- chakra.ui/theme/typography/fonts.ts
+++ chakra.ui/theme/typography/fonts.ts
@@ -1,10 +1,21 @@
+/* eslint-disable quotes */
 import { theme as base, type ChakraTheme } from '@chakra-ui/react';
 
+const systemSerifFallbacks = `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+  Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", "Georgia", "Times New Roman", serif`;
+
+const commonSerifFallbacks = `Cambria, "Times New Roman", serif`;
+
+const systemSansSerifFallbacks = `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+  Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif`;
+
+const commonSansSerifFallbacks = `Roboto, Arial, sans-serif`;
+
 export const fonts: ChakraTheme['fonts'] = {
-  heading: `'Bebas Neue', cursive`,
-  body: `Recursive, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
-  "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif,
-  "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
-  default: `-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-  Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;`,
+  heading: `'Bebas Neue', cursive` + commonSansSerifFallbacks,
+  body:
+    `Recursive` + `, ${commonSansSerifFallbacks}, ${commonSansSerifFallbacks}`,
+  brand: `'Fredoka One', ${commonSansSerifFallbacks}`,
+  accent: `'Text Me One', ${commonSansSerifFallbacks}`,
+  default: `${commonSansSerifFallbacks}, ${systemSansSerifFallbacks}`,
 };
